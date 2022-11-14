47e6e0f1ddcdd529e51efdf7c7fd53e35ff19c05
edwin-wq <winisgomez@gmail.com>
2022-11-14 02:24:30 -0600

    error de imagenes solucioado y primeras navegaciones entre ventanas

---
+++ app/src/main/java/com/example/commovproyecto/LoginModeradoresActivity.java
@@ -0,0 +1,14 @@
+package com.example.commovproyecto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginModeradoresActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_moderadores);
+    }
+}
--- app/src/main/java/com/example/commovproyecto/MainActivity.java
+++ app/src/main/java/com/example/commovproyecto/MainActivity.java
@@ -7,41 +7,53 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     EditText usuario, constrasena;
     Button btningresa, btnloginMod;
+    ImageButton imgBtnsalir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        usuario = findViewById(R.id.editTextUser);
-        constrasena = findViewById(R.id.editTextPassword);
-        btningresa = findViewById(R.id.buttonIngresar);
-        btnloginMod =findViewById(R.id.buttonIniciarModeradores);
+        usuario = (EditText) findViewById(R.id.editTextUser);
+        constrasena = (EditText) findViewById(R.id.editTextPassword);
+        btningresa = (Button) findViewById(R.id.buttonIngresar);
+        btnloginMod =(Button) findViewById(R.id.buttonIniciarModeradores);
+        imgBtnsalir = (ImageButton)findViewById(R.id.imageButtonSalir);
 
-        btningresa.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        btningresa.setOnClickListener(this);
+        btnloginMod.setOnClickListener(this);
+        imgBtnsalir.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.buttonIngresar:
                 if(usuario.getText().toString().equals("Admin")&& constrasena.getText().toString().equals("0519gomez")){
                     startActivity(new Intent(MainActivity.this, MenuActivity.class));
                 }
                 else {
                     Toast.makeText(MainActivity.this,"Usuario o contraseña incorrecta", Toast.LENGTH_LONG).show();
                 }
-            }
-        });
+                break;
+            case R.id.buttonIniciarModeradores:
+                //startActivity(new Intent(MainActivity.this, LoginModeradoresActivity.class));
+                startActivity(new Intent(MainActivity.this, MnModeradoresActivity.class));
 
-        btnloginMod.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, InicioModeradoresActivity.class));
-            }
-        });
+                break;
+            case R.id.imageButtonSalir:
 
+        }
     }
 }
+
+
+
